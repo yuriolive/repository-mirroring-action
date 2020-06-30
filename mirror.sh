a76737509076488e64ee73e455e90c3a563e9fad
Yuri Olive <yuriolive@users.noreply.github.com>
2020-06-30 16:10:41 -0300

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -5,4 +5,4 @@ set -eu
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
-git push --force --prune mirror master
+git push --force --prune mirror HEAD:master
